ad24f5ec9752bdb3115604e7e809cd0ea8ace472
maximesraiki <maximes@theodo.fr>
2016-02-24 21:29:25 +0100

    angular ?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-angular.module('pepiniere-gpc.home').controller('HomeMainCtrl', function($scope) {
+controller('HomeMainCtrl', function($scope) {
   etOuais = function () {
     console.log('coucou')
   }
