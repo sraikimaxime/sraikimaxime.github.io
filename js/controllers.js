9ec480ff4b8f01f3fc02b683c7da0af3c19814de
maximesraiki <maximes@theodo.fr>
2016-02-24 22:20:15 +0100

    hehe

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,4 @@ angular.module('myApp.controllers', []).
 controller('myAppController', function($scope) {
     $scope.initialList = [1, 2, 3, 4];
     $scope.lodashList = _.chunk($scope.initialList, 2);
-    $scope.anoherLodashList = _.sortBy([
-      { 'name': 'fred', 'age': 48 },
-      { 'name': 'barney', 'age': 36 },
-      { 'name': 'cleme', 'age': 42 },
-      { 'name': 'max', 'age': 34 }
-    ], ['name', 'age']);
 });
