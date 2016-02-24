ad6b9b7ce522417c85b62399bcd6ed53dbceb7c7
maximesraiki <maximes@theodo.fr>
2016-02-24 22:08:35 +0100

    lodash git add .

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,12 @@
 angular.module('myApp.controllers', []).
 controller('myAppController', function($scope) {
+    var users = [
+      { 'user': 'fred', 'age': 48 },
+      { 'user': 'barney', 'age': 36 },
+      { 'user': 'cleme', 'age': 42 },
+      { 'user': 'max', 'age': 34 }
+    ];
     $scope.initialList = [1, 2, 3, 4];
     $scope.lodashList = _.chunk($scope.initialList, 2);
+    $scope.anoherLodashList = _.sortBy(users, function(o) { return o.age; });
 });
