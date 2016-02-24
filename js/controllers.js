ebbef95c74e422b1c6d1a68d49cc2be4a16110a2
maximesraiki <maximes@theodo.fr>
2016-02-24 21:59:58 +0100

    heho

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('myApp.controllers', []).
-controller('myAppController', function($scope, _) {
+controller('myAppController', function($scope) {
     $scope.initialList = [1, 2, 3, 4];
     $scope.lodashList = _.chunk($scope.initialList, 2);
 });
